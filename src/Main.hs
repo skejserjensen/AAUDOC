39f404f4d7050268447c0f34f25033a20d90e0a9
Soeren Kejser Jensen <devel@kejserjensen.dk>
2014-05-08 18:53:09 +0200

    Checked package with hlint and scan

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 import System.Environment (getArgs)
 import Control.Monad (filterM, (>=>))
 import Data.Time.Clock.POSIX (getPOSIXTime)
-import System.Exit (ExitCode(..), exitFailure)
+import System.Exit (ExitCode (..), exitFailure)
 import System.Directory (setCurrentDirectory, doesFileExist)
 import System.FilePath (takeExtension, dropExtension, takeDirectory)
 
@@ -26,7 +26,7 @@ processDocument docPath = do
             putStrLn $ formatPrints "BEGIN" docPath
             startTimeStamp <- getPOSIXTime
             -- Changes the current working directory to where the document is
-            setCurrentDirectory docFolder 
+            setCurrentDirectory docFolder
             -- Parses the header of the current file
             jobList <- buildJobList document
             -- Run each job read from the document header
@@ -34,7 +34,7 @@ processDocument docPath = do
             -- Outputs the complete compile time for the documnent at the end
             endTimeStamp <- getPOSIXTime
             putStrLn $ formatPrints "END" $ show $ endTimeStamp - startTimeStamp
-            return () 
+            return ()
 
 performJob :: Document -> Job -> IO (ExitCode, String, String)
 performJob document (Job jobOperation jobFunction) = printJob >> jobFunction document
@@ -53,7 +53,7 @@ couldBeTex :: String -> Bool -- Checks if a document could be a Tex documents
 couldBeTex docPath = takeExtension docPath `elem` ["", ".", ".tex"]
 
 expandTexPath :: String -> String -- Loosens the requirements of document paths
-expandTexPath documentPath = case (last documentPath) of 
+expandTexPath documentPath = case last documentPath of
                                 'x' -> documentPath
                                 '.' -> documentPath ++ "tex"
                                 _ -> documentPath ++ ".tex"
@@ -63,7 +63,7 @@ formatPrints "BEGIN" fileName = "-- Processing: " ++ fileName
 formatPrints "JOB" printOperation = "   " ++ printOperation
 formatPrints "ERROR" exitCode = "     Job terminated with error code: " ++ exitCode
 formatPrints "END" compileTime = "   [Time elapsed]: " ++ compileTime ++ "\n"
-formatPrints _  input = error $ "FormatPrints: unknown argument to print formatter \"" ++ input ++ "\""
+formatPrints _ input = error $ "FormatPrints: unknown argument to print formatter \"" ++ input ++ "\""
 
 printErrorCode :: Int -> IO ()
 printErrorCode exitCode = putStrLn (formatPrints "ERROR" $ show exitCode)
@@ -73,6 +73,6 @@ printError "" = return ()
 printError output = putStr (indentJobOutput (errorBorder ++ ('\n' : output) ++ errorBorder)) >> waitForUserInput
     where errorBorder = "-----------------------------------" -- getChar is a hack to pause exection
           waitForUserInput = putStrLn "     Press Return to to continue, or Ctrl-C to terminate" >> getChar >> return ()
-        
+
 indentJobOutput :: String -> String
-indentJobOutput output = unlines $ map ("     "++) $ lines output
+indentJobOutput output = unlines $ map ("     " ++) $ lines output
