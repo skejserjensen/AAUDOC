dbd2f8e1fa78c814387920a37d32c4e5fe36e9e4
Soeren Kejser Jensen <devel@kejserjensen.dk>
2015-02-01 19:13:16 +0100

    Added more robust detection of aaudoc headers in tex files

--- src/File.hs
+++ src/File.hs
@@ -9,7 +9,9 @@ import Datatypes (Document (..), Job (..))
 
 -- Global Level Imports --
 import Data.Char (isSpace)
+import Data.List (isPrefixOf)
 import Control.Monad (liftM)
+import System.IO (withFile, IOMode(ReadMode), hGetContents)
 
 -- Public Functions --
 buildJobList :: Document -> IO [Job]
@@ -17,9 +19,15 @@ buildJobList doc = liftM (parseHeader . expandMacros doc) $ readHeader $ path do
 
 -- Header Parsing Functions --
 readHeader :: String -> IO [String]
-readHeader docPath = liftM takeHeader $ readFile docPath
+readHeader docPath = withFile docPath ReadMode $ \docFile -> do
+                       -- readFile is not used to ensure the file handle is closed
+                       docStr <- hGetContents docFile
+                       let header = takeHeader docStr
+                       header `seq` return header
     where takeHeader = takeWhile couldBeHeader . rstripWhitespace . lines
-          couldBeHeader line = not (null line) && head line == '%'
+          couldBeHeader line = foldl (\acc job -> job `isPrefixOf` line || acc)
+            -- Must contain all types of jobs and macro any could appear in the header
+            False ["%command",  "%link", "%link-doc", "%clean", "%macro"]
 
 parseHeader :: [String] -> [Job]
 parseHeader headerLines = reverse $ foldl buildJobs [] headerLines
